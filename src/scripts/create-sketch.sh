8d8696ca486b069d41efea4b5b0f25812548e441
Thierry R. Le Bourdais <thierry@orphic.ca>
2022-06-03 19:12:42 -0400

    Added script to create sketches

---
+++ src/scripts/create-sketch.sh
@@ -0,0 +1,29 @@
+# create sketch dir
+mkdir src/sketches/$1
+
+# create sketch component
+echo "import p5 from 'p5';
+const $1 = (sketchArgs:any) => (p:any) => {
+  // eslint-disable-next-line no-param-reassign
+  p.setup = () => {
+  };
+
+  // eslint-disable-next-line no-param-reassign
+  p.draw = () => {
+  };
+};
+
+export default $1;" > src/sketches/$1/$1.ts
+
+# create sketch config file
+echo "import $1 from './$1';
+
+export default {
+  id: '',
+  sketch: $1,
+  sketchArgs: {
+  },
+  isResponsive: false,
+  showHeader: true,
+  title: '$1',
+};" > src/sketches/$1/$1Conf.ts
