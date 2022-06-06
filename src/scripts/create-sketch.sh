af428e3ff3075b05d67775cb8ed1fc155b1dba85
Thierry R. Le Bourdais <thierry@orphic.ca>
2022-06-06 09:39:42 -0400

    Updated script

--- src/scripts/create-sketch.sh
+++ src/scripts/create-sketch.sh
@@ -16,16 +16,16 @@ const $1 = (sketchArgs:any) => (p:any) => {
 export default $1;" > src/sketches/$1/$1.ts
 
 # create sketch config file
-echo "import $1 from './$1';
+echo "import SketchModel from '../../models/SketchModel';
+import $1 from './$1';
 
-const $1 : SketchModel = {
+const $1Conf : SketchModel = {
   id: '$1',
   sketch: $1,
   sketchArgs: {
   },
-  isResponsive: false,
   showHeader: true,
   title: '$1',
 };
 
-export default $1;" > src/sketches/$1/$1Conf.ts
+export default $1Conf;" > src/sketches/$1/$1Conf.ts
